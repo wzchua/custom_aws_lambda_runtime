c70dcb725e42070ff0904e8e0c748f93c20e900f
WeiZheng <13881045+wzchua@users.noreply.github.com>
2022-12-18 12:42:21 +0800

    make run.sh chmod +x

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -v ~/.aws-lambda-rie:/aws-lambda -p 9000:8080 --entrypoint /aws-lambda/aws-lambda-rie  test:latest  /var/task/AwsLambdaRuntimeNative 
+docker run -v ~/.aws-lambda-rie:/aws-lambda -e WRITE_INDENTED_JSON=true -p 9000:8080 --entrypoint /aws-lambda/aws-lambda-rie  test:latest  /var/task/AwsLambdaRuntimeNative 
